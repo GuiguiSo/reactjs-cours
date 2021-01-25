c5e3279ee191f54acaee52a1bb9bcffec703b7d7
Nicolas Klein <mkleinnicolas@gmail.com>
2021-01-25 18:08:02 +0100

    Homepage modif

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from "./views/HomePage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics end point. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -5,38 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Button from 'react-bootstrap/Button';
 
 
-class App extends React.Component {
+class HomePage extends React.Component {
 
     constructor(props){
         super(props)
-
-        this.state = {
-            user: ['A','B','C'],
-            name : 'Guillaume'
-        }
-    }
-
-    componentDidMount(){
-        console.log(this.state.name)
-        this.setState({
-            name : 'Guillaume Le tricheur'
-        }, () => console.log('call back :' , this.state.name))
-
     }
 
     render() {
         return (
-            <div className="App">
-                {
-                    this.state.user.map((user,i) => <p key={i}>{user}</p>)
-                }
-                <Casino casinos = {[{name: 'Monsupercasino', url:'httpmdr'}, {name: 'Moncasino', url:'httplolmdr'}]}></Casino>
-                <Button variant="danger">Link</Button>{' '}
-                <Button variant="primary">Primary</Button>
-            </div>
+            <p>Texte pour rien</p>
         );
     }
 
 }
 
-export default App;
+export default HomePage;
